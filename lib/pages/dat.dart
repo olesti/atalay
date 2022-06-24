a385cd180bc91639705a1563ec861aa63bb92d25
olesti <ataly08@hotmail.com>
2022-06-24 12:00:59 +0300

    23haz tez öncesi

--- lib/pages/dat.dart
+++ lib/pages/dat.dart
@@ -23,7 +23,7 @@ class _nameState extends State<name> {
         body: Column(
           children: [
             Container(
-              height: 200,
+              height: 180,
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.only(
                   bottomRight: Radius.circular(50),
@@ -33,7 +33,7 @@ class _nameState extends State<name> {
               child: Stack(
                 children: [
                   Positioned(
-                      top: 80,
+                      top: 60,
                       left: 0,
                       child: Container(
                         height: 70,
@@ -45,7 +45,7 @@ class _nameState extends State<name> {
                                 bottomRight: Radius.circular(50))),
                       )),
                   Positioned(
-                      top: 90,
+                      top: 70,
                       left: 20,
                       child: Text(
                           "Informationen für vor und \nnach dem ERDBEBEN",
@@ -214,7 +214,369 @@ class _nameState extends State<name> {
                 ],
               ),
             )),
+            new Card(
+                child: Container(
+              height: 100,
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: 35,
+                      left: 20,
+                      child: Material(
+                          child: Container(
+                        height: 180.0,
+                        width: width * 0.9,
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 252, 252, 252),
+                            borderRadius: BorderRadius.circular(0.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(186, 0, 255, 191)
+                                    .withOpacity(0.3),
+                                offset: new Offset(-10, 10),
+                                blurRadius: 15.0,
+                                spreadRadius: 1.0,
+                              )
+                            ]),
+                      ))),
+                  Positioned(
+                      top: 55,
+                      left: 30,
+                      child: Card(
+                          color: Colors.amber,
+                          elevation: 10.0,
+                          //shadowColor: Colors.grey.withOpacity(0.5),
+                          /*shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),*/
+                          child: Container(
+                              height: 135,
+                              width: 112.5,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: DecorationImage(
+                                  //fit: BoxFit.fill,
+                                  image: AssetImage("assets/images/dunya1.png"),
+                                ),
+                              )))),
+                  Positioned(
+                      top: 45,
+                      left: 160,
+                      child: Container(
+                        height: 150,
+                        width: 180,
+                        child: Column(children: [
+                          Text(
+                            "Was tun in der Zeit eines Erdbebens",
+                            style: TextStyle(
+                                fontSize: 19,
+                                color: Color(0xFF363f93),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Divider(
+                            color: Colors.black,
+                          ),
+                          Text(
+                            "Orte, an denen man sich bei einem Erdbeben verstecken kann, was danach zu tun ist",
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.normal),
+                          ),
+                        ]),
+                      ))
+                ],
+              ),
+            )),
           ],
         ));
+    /* //burası scroll denemesi
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                ),
+                color: Color(0xFF363f93),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: 80,
+                      left: 0,
+                      child: Container(
+                        height: 70,
+                        width: 300,
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(50),
+                                bottomRight: Radius.circular(50))),
+                      )),
+                  Positioned(
+                      top: 90,
+                      left: 20,
+                      child: Text(
+                          "Informationen für vor und \nnach dem ERDBEBEN",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Color.fromARGB(255, 0, 99, 165))))
+                ],
+              ),
+            ),
+            /*SizedBox(
+              height: height + 0.05,
+            ),*/
+            ListView.builder(
+              physics: ScrollPhysics(parent: null),
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Column(children: <Widget>[
+                  InkWell(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (BuildContext context) => Asall(),
+                        ));
+                      },
+                      child: Container(
+                        height: 230,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                top: 35,
+                                left: 20,
+                                child: Material(
+                                    child: Container(
+                                  height: 180.0,
+                                  width: width * 0.9,
+                                  decoration: BoxDecoration(
+                                      color: Color.fromARGB(255, 252, 252, 252),
+                                      borderRadius: BorderRadius.circular(0.0),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color:
+                                              Color.fromARGB(186, 0, 255, 191)
+                                                  .withOpacity(0.3),
+                                          offset: new Offset(-10, 10),
+                                          blurRadius: 15.0,
+                                          spreadRadius: 1.0,
+                                        )
+                                      ]),
+                                ))),
+                            Positioned(
+                                top: 55,
+                                left: 30,
+                                child: Card(
+                                    color: Colors.amber,
+                                    elevation: 10.0,
+                                    //shadowColor: Colors.grey.withOpacity(0.5),
+                                    /*shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),*/
+                                    child: Container(
+                                        height: 135,
+                                        width: 112.5,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          image: DecorationImage(
+                                            //fit: BoxFit.fill,
+                                            image: AssetImage(
+                                                "assets/images/dunya3.png"),
+                                          ),
+                                        )))),
+                            Positioned(
+                                top: 45,
+                                left: 160,
+                                child: Container(
+                                  height: 150,
+                                  width: 180,
+                                  child: Column(children: [
+                                    Text(
+                                      "Vorbereitungen vor dem Erdbeben",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Color(0xFF363f93),
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Divider(
+                                      color: Colors.black,
+                                    ),
+                                    Text(
+                                      "Notfalltasche usw.",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.grey,
+                                          fontWeight: FontWeight.normal),
+                                    ),
+                                  ]),
+                                ))
+                          ],
+                        ),
+                      )),
+                  InkWell(
+                      child: Container(
+                    height: 230,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            top: 35,
+                            left: 20,
+                            child: Material(
+                                child: Container(
+                              height: 180.0,
+                              width: width * 0.9,
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 252, 252, 252),
+                                  borderRadius: BorderRadius.circular(0.0),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Color.fromARGB(186, 0, 255, 191)
+                                          .withOpacity(0.3),
+                                      offset: new Offset(-10, 10),
+                                      blurRadius: 15.0,
+                                      spreadRadius: 1.0,
+                                    )
+                                  ]),
+                            ))),
+                        Positioned(
+                            top: 55,
+                            left: 30,
+                            child: Card(
+                                color: Colors.amber,
+                                elevation: 10.0,
+                                //shadowColor: Colors.grey.withOpacity(0.5),
+                                /*shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),*/
+                                child: Container(
+                                    height: 135,
+                                    width: 112.5,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      image: DecorationImage(
+                                        //fit: BoxFit.fill,
+                                        image: AssetImage(
+                                            "assets/images/dunya1.png"),
+                                      ),
+                                    )))),
+                        Positioned(
+                            top: 45,
+                            left: 160,
+                            child: Container(
+                              height: 150,
+                              width: 180,
+                              child: Column(children: [
+                                Text(
+                                  "Was tun in der Zeit eines Erdbebens",
+                                  style: TextStyle(
+                                      fontSize: 19,
+                                      color: Color(0xFF363f93),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Divider(
+                                  color: Colors.black,
+                                ),
+                                Text(
+                                  "Orte, an denen man sich bei einem Erdbeben verstecken kann, was danach zu tun ist",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.normal),
+                                ),
+                              ]),
+                            ))
+                      ],
+                    ),
+                  )),
+                  InkWell(
+                      child: Container(
+                    height: 230,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            top: 35,
+                            left: 20,
+                            child: Material(
+                                child: Container(
+                              height: 180.0,
+                              width: width * 0.9,
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 252, 252, 252),
+                                  borderRadius: BorderRadius.circular(0.0),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Color.fromARGB(186, 0, 255, 191)
+                                          .withOpacity(0.3),
+                                      offset: new Offset(-10, 10),
+                                      blurRadius: 15.0,
+                                      spreadRadius: 1.0,
+                                    )
+                                  ]),
+                            ))),
+                        Positioned(
+                            top: 55,
+                            left: 30,
+                            child: Card(
+                                color: Colors.amber,
+                                elevation: 10.0,
+                                //shadowColor: Colors.grey.withOpacity(0.5),
+                                /*shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),*/
+                                child: Container(
+                                    height: 135,
+                                    width: 112.5,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      image: DecorationImage(
+                                        //fit: BoxFit.fill,
+                                        image: AssetImage(
+                                            "assets/images/dunya1.png"),
+                                      ),
+                                    )))),
+                        Positioned(
+                            top: 45,
+                            left: 160,
+                            child: Container(
+                              height: 150,
+                              width: 180,
+                              child: Column(children: [
+                                Text(
+                                  "Was tun in der Zeit eines Erdbebens",
+                                  style: TextStyle(
+                                      fontSize: 19,
+                                      color: Color(0xFF363f93),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Divider(
+                                  color: Colors.black,
+                                ),
+                                Text(
+                                  "Orte, an denen man sich bei einem Erdbeben verstecken kann, was danach zu tun ist",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.normal),
+                                ),
+                              ]),
+                            ))
+                      ],
+                    ),
+                  ))
+                ]);
+              },
+              itemCount: 1,
+            ),
+          ],
+        ));*/
   }
 }
